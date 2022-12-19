a56a835d3a517302176fd220eea28750ff98f005
akd420 <93831664+akd420@users.noreply.github.com>
2022-12-19 22:00:14 +0600

    Create selection.cpp

---
+++ selection.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,x;
+    cout << "Number of elements : ";
+    cin >> n;
+    int array[n];
+    for(int i = 0; i < n;i++){
+        x =i;
+        cout <<"Insert element " << x << " : ";
+        cin >> array[i];
+    }
+    for(int i = 0; i < n-1;i++){
+        {
+            for(int j = i+1; j <n;j++){
+                if(array[j]<array[i]){
+                int temp = array[j];
+                array[j] = array[i];
+                array[i] = temp;
+            }
+        }
+        
+    }
+   
+}
+     cout <<"Sorted elements : ";
+    for (int i = 0; i < n; i++){
+        cout << array[i] << " ";
+    }
+}
