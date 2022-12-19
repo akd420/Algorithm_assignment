9afac673cb9b9edd48e64e0e1370365b69690019
akd420 <93831664+akd420@users.noreply.github.com>
2022-12-19 21:15:13 +0600

    Create insertions.cpp

---
+++ insertions.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Number of elements : ";
+    cin >> n;
+    int array[n];
+    for(int i = 0; i < n;i++){
+        cin >> array[i];
+    }
+    for(int i = 1; i < n;i++){
+        int arr = array[i];
+        int j = i-1;
+        while(array[j]>arr && j>=0){
+            array[j+1] = array[j];
+            j--;
+        }
+        array[j+1]=arr;
+        
+    }
+    for (int i = 0; i < n; i++){
+        cout << array[i] << " ";
+    }
+}
