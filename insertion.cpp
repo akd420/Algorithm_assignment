8a7e4357cf82749201a7790177da650c3c08ed05
akd420 <93831664+akd420@users.noreply.github.com>
2022-12-19 21:29:48 +0600

    Update insertion.cpp

--- insertion.cpp
+++ insertion.cpp
@@ -1,23 +1,26 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
+    int n,x;
     cout << "Number of elements : ";
     cin >> n;
     int array[n];
     for(int i = 0; i < n;i++){
+        x =i;
+        cout <<"Insert element " << x << ":";
         cin >> array[i];
     }
     for(int i = 1; i < n;i++){
-        int arr = array[i];
+        int temp = array[i];
         int j = i-1;
-        while(array[j]>arr && j>=0){
+        while(array[j]>temp && j>=0){
             array[j+1] = array[j];
             j--;
         }
-        array[j+1]=arr;
+        array[j+1]=temp;
         
     }
+    cout <<"Sorted elements :";
     for (int i = 0; i < n; i++){
         cout << array[i] << " ";
     }
